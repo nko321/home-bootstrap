48ad9101d678dfbd3921bd62ab04ef8dc9009b7c
Nathan Olberding <nathan.olberding@gmail.com>
2018-10-13 20:53:54 -0700

    Adding color to powershell.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,39 @@
 Set-PSReadlineOption -BellStyle None
 $Host.PrivateData.ErrorForegroundColor = "Gray"
-Set-PSReadlineOption -TokenKind Command -ForegroundColor "DarkRed"
+$options = Get-PSReadlineOption
 [console]::ForegroundColor = "DarkGreen"
 [console]::BackgroundColor = "White"
+
+$options.DefaultTokenForegroundColor            = "DarkCyan"
+$options.CommentForegroundColor                 = "DarkGray"
+$options.KeywordForegroundColor                 = "Magenta"
+$options.StringForegroundColor                  = "DarkCyan"
+$options.OperatorForegroundColor                = "Magenta"
+$options.VariableForegroundColor                = "Green"
+$options.CommandForegroundColor                 = "DarkRed"
+$options.ParameterForegroundColor               = "Red"
+$options.TypeForegroundColor                    = "Gray"
+$options.NumberForegroundColor                  = "Blue"
+$options.MemberForegroundColor                  = "Black"
+$options.ErrorForegroundColor                   = "Red"
+$options.EmphasisForegroundColor                = "Cyan"
+$options.ContinuationPromptForegroundColor      = "Gray"
+
+$options.DefaultTokenBackgroundColor            = "White"
+$options.CommentBackgroundColor                 = "DarkYellow"
+$options.KeywordBackgroundColor                 = "White"
+$options.StringBackgroundColor                  = "White"
+$options.OperatorBackgroundColor                = "White"
+$options.VariableBackgroundColor                = "White"
+$options.CommandBackgroundColor                 = "White"
+$options.ParameterBackgroundColor               = "White"
+$options.TypeBackgroundColor                    = "White"
+$options.NumberBackgroundColor                  = "White"
+$options.MemberBackgroundColor                  = "White"
+$options.EmphasisBackgroundColor                = "DarkMagenta"
+$options.ErrorBackgroundColor                   = "DarkMagenta"
+$options.ContinuationPromptBackgroundColor      = "White"
+
 function grep ($pattern){
         return (Get-ChildItem -Recurse *.* | Select-String -Pattern $pattern | Select-Object -Unique Path)
 }
--- start.ps1
+++ start.ps1
@@ -2,4 +2,4 @@ Copy-Item -Path Microsoft.PowerShell_profile.ps1 -Destination $profile
 Set-ExecutionPolicy Unrestricted -Force
 dism /online /Enable-Feature /FeatureName:TelnetClient
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install autohotkey virtualbox 7zip notepadplusplus putty vscode inkscape vlc sumatrapdf windirstat git -y
+choco install autohotkey virtualbox 7zip notepadplusplus putty vscode inkscape vlc sumatrapdf windirstat git openssh -y
