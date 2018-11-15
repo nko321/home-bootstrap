03bd651a414a1fec05f2ad601ca96fdc8eef047f
Nathan Olberding <nathan.olberding@gmail.com>
2018-11-15 12:09:43 -0800

    Fixing profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,44 +1,42 @@
 Set-PSReadlineOption -BellStyle None
-$Host.PrivateData.ErrorForegroundColor = "Gray"
-$options = Get-PSReadlineOption
-[console]::ForegroundColor = "DarkGreen"
-[console]::BackgroundColor = "White"
+#$Host.PrivateData.ErrorForegroundColor = "Gray"
+#$options = Get-PSReadlineOption
+#[console]::ForegroundColor = "DarkGreen"
+#[console]::BackgroundColor = "White"
 
-$options.DefaultTokenForegroundColor            = "DarkCyan"
-$options.CommentForegroundColor                 = "DarkGray"
-$options.KeywordForegroundColor                 = "Magenta"
-$options.StringForegroundColor                  = "DarkCyan"
-$options.OperatorForegroundColor                = "Magenta"
-$options.VariableForegroundColor                = "Green"
-$options.CommandForegroundColor                 = "DarkRed"
-$options.ParameterForegroundColor               = "Red"
-$options.TypeForegroundColor                    = "Gray"
-$options.NumberForegroundColor                  = "Blue"
-$options.MemberForegroundColor                  = "Black"
-$options.ErrorForegroundColor                   = "Red"
-$options.EmphasisForegroundColor                = "Cyan"
-$options.ContinuationPromptForegroundColor      = "Gray"
-
-$options.DefaultTokenBackgroundColor            = "White"
-$options.CommentBackgroundColor                 = "DarkYellow"
-$options.KeywordBackgroundColor                 = "White"
-$options.StringBackgroundColor                  = "White"
-$options.OperatorBackgroundColor                = "White"
-$options.VariableBackgroundColor                = "White"
-$options.CommandBackgroundColor                 = "White"
-$options.ParameterBackgroundColor               = "White"
-$options.TypeBackgroundColor                    = "White"
-$options.NumberBackgroundColor                  = "White"
-$options.MemberBackgroundColor                  = "White"
-$options.EmphasisBackgroundColor                = "DarkMagenta"
-$options.ErrorBackgroundColor                   = "DarkMagenta"
-$options.ContinuationPromptBackgroundColor      = "White"
+#$options.DefaultTokenForegroundColor            = "DarkCyan"
+#$options.CommentForegroundColor                 = "DarkGray"
+#$options.KeywordForegroundColor                 = "Magenta"
+#$options.StringForegroundColor                  = "DarkCyan"
+#$options.OperatorForegroundColor                = "Magenta"
+#$options.VariableForegroundColor                = "Green"
+#$options.CommandForegroundColor                 = "DarkRed"
+#$options.ParameterForegroundColor               = "Red"
+#$options.TypeForegroundColor                    = "Gray"
+#$options.NumberForegroundColor                  = "Blue"
+#$options.MemberForegroundColor                  = "Black"
+#$options.ErrorForegroundColor                   = "Red"
+#$options.EmphasisForegroundColor                = "Cyan"
+#$options.ContinuationPromptForegroundColor      = "Gray"
+#
+#$options.DefaultTokenBackgroundColor            = "White"
+#$options.CommentBackgroundColor                 = "DarkYellow"
+#$options.KeywordBackgroundColor                 = "White"
+#$options.StringBackgroundColor                  = "White"
+#$options.OperatorBackgroundColor                = "White"
+#$options.VariableBackgroundColor                = "White"
+#$options.CommandBackgroundColor                 = "White"
+#$options.ParameterBackgroundColor               = "White"
+#$options.TypeBackgroundColor                    = "White"
+#$options.NumberBackgroundColor                  = "White"
+#$options.MemberBackgroundColor                  = "White"
+#$options.EmphasisBackgroundColor                = "DarkMagenta"
+#$options.ErrorBackgroundColor                   = "DarkMagenta"
+#$options.ContinuationPromptBackgroundColor      = "White"
 
 function grep ($pattern){
         return (Get-ChildItem -Recurse *.* | Select-String -Pattern $pattern | Select-Object -Unique Path)
 }
-Clear-Host
-Start-Transcript -Append -Path "~\$(Get-Date -format "yyyy-MM-dd_HH:mm:ss").pstranscript.txt"
 
 Function Play-Sound
 {
@@ -63,3 +61,6 @@ Function Touch-File
         echo $null > $file
     }
 }
+
+Clear-Host
+Start-Transcript
